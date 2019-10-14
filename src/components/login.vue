8ca5a6485119721457ddd6a8b7633aed599b2190
gfj0120 <502867612@qq.com>
2019-10-14 21:19:27 +0800

    下载了element ui

---
+++ src/components/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login">
+      <el-button>默认按钮</el-button>
+      <el-button circle type="danger">主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
